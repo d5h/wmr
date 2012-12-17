6efeae9e897e0e69d305358654f5957b081f2f91
Dan Hipschman <dsh@linux.ucla.edu>
2012-12-16 21:18:23 -0800

    remove scalar checks

--- wmr.py
+++ wmr.py
@@ -13,28 +13,9 @@ class WatermarkRemover(object):
         self.mat = image
         self.detect_constants()
 
-    def detect_alphas(self):
-        m = self.mat
-        self.alphas = set()
-        for r in range(m.rows):
-            for c in range(m.cols):
-                if c == 0:
-                    x, y, z = m[r, c]
-                else:
-                    nx, ny, nz = m[r, c]
-                    if x and y and z and round(nx / x, 3) == round(ny / y, 3) == round(nz / z, 3):
-                        self.alphas.add(round(nx / x, 3))
-                        if round(nx / x, 3) not in (0.0, 1.0):
-                            m[r, c] = (256.0, 0.0, 0.0)
-                    x, y, z = nx, ny, nz
-        self.alphas -= {0.0, 1.0}
-        cv.NamedWindow('demo', cv.CV_WINDOW_AUTOSIZE)
-        cv.ShowImage('demo', m)
-        cv.WaitKey()
-
     def detect_constants(self):
         m = self.mat
-        self.constants = set()
+        const = 0
         for r in range(m.rows):
             for c in range(m.cols):
                 if c == 0:
@@ -43,9 +24,13 @@ class WatermarkRemover(object):
                     nx, ny, nz = m[r, c]
                     if self.approx_same(nx - x, ny - y, nz - z):
                         a = ave(nx - x, ny - y, nz - z)
-                        if 5 < a:
-                            self.constants.add(a)
-                            m[r, c] = (0., 0., 255.)
+                        if 5 < a < 30:
+                            if self.approx_same(-const, a):
+                                const = 0
+                            else:
+                                const = a
+                    if const:
+                        m[r, c] = (nx - const, ny - const, nz - const)
                     x, y, z = nx, ny, nz
         cv.NamedWindow('demo', cv.CV_WINDOW_AUTOSIZE)
         cv.ShowImage('demo', m)
